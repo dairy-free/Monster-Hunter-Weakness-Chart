6e16b0492f1b7c4d5ab51e5ac7102b5309277eac
dairy-free <67081832+dairy-free@users.noreply.github.com>
2021-01-16 15:42:44 -0800

    Monster Hunter World Weakness Chart

--- dist/js/app.js
+++ dist/js/app.js
@@ -4,13 +4,28 @@ async function getMonster(monster) {
   const res = await fetch(`${apiUrl}/22`);
   const data = await res.json();
   console.log(data);
+
+  const url = new URL('https://mhw-db.com/monsters');
+  url.searchParams.set('p', JSON.stringify({
+    id: true,
+    name: true,
+    type: true,
+    weaknesses: true,
+}));
+
+fetch(url)
+    .then(response => response.json())
+    .then(weapons => {
+      console.log(weapons)
+    });
+
+
+
 }
 
   getMonster();
 
   // Selectors 
-  // insert selector for image here
+  const monsterIcon = document.querySelectorAll('.monster-image-lp')[0];
   const monsterTitle = document.querySelectorAll('.monster-name')[0];
   const monsterWeakness = document.querySelectorAll('.monster-weakness')[0];
-  console.log(monsterWeakness);
-  console.log(monsterTitle);
