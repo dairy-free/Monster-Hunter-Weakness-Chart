da70087a7e0652d3906d4759af5586ba8445ca5b
dairy-free <67081832+dairy-free@users.noreply.github.com>
2021-01-17 01:25:02 -0800

    Monster Hunter Weakness Chart

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,24 +1,39 @@
 const apiUrl = "https://mhw-db.com/monsters";
+const monster = `Anjanath`;
+
+// Selectors 
+const monsterIcon = document.querySelectorAll('.monster-image-lp')[0];
+const monsterTitle = document.querySelectorAll('.monster-name')[0];
+const monsterWeakness = document.querySelectorAll('.monster-weakness')[0];
 
 async function getMonster() {
   const url = new URL('https://mhw-db.com/monsters');
   url.searchParams.set('p', JSON.stringify({
     id: true,
     name: true,
-    type: true,
+    // type: true,
     weaknesses: true,
 }));
 
   const res = await fetch(url)
     const data = await res.json()
     console.log(data);
+  
+  
+  const monsterName = data.find((item) => {
+    return item.name === `${monster}`;
+  })
 
+  console.log(monsterName)
+  console.log(monsterName.name)
+  console.log(monsterName.weaknesses)
   
 }
 
-  getMonster();
+function updateMonsterData() {
+  
+}
+console.log(apiUrl);
+getMonster();
 
-  // Selectors 
-  const monsterIcon = document.querySelectorAll('.monster-image-lp')[0];
-  const monsterTitle = document.querySelectorAll('.monster-name')[0];
-  const monsterWeakness = document.querySelectorAll('.monster-weakness')[0];
+ 
