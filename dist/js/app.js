c5ed86d98d5332c3d6101a7ec60af772e92a0784
dairy-free <67081832+dairy-free@users.noreply.github.com>
2021-01-18 19:16:20 -0800

    Monster Hunter Weakness Chart

--- dist/js/app.js
+++ dist/js/app.js
@@ -8,7 +8,7 @@ const monsterName = document.querySelectorAll('.monster-name')[0];
 const monsterWeakness = document.querySelectorAll('.monster-weakness')[0];
 
 
-async function getMonster(testName, weakness) {
+async function getMonster() {
   const url = new URL('https://mhw-db.com/monsters');
   url.searchParams.set('p', JSON.stringify({
     id: true,
@@ -25,27 +25,22 @@ async function getMonster(testName, weakness) {
     return item.name === `${monster}`;
   })
 
+  updateMonsterData(monsterData);
+}
+
+
+
+function updateMonsterData(monsterData) {
   monsterImage.innerHTML = ``;
   monsterName.innerHTML = `${monsterData.name}`;
-  monsterWeakness.innerHTML = `<p>${monsterData.weaknesses[0].element}</p>`;
-
-
+  monsterWeakness.innerHTML = `<p>${monsterData.weaknesses[0].element}${monsterData.weaknesses[0].stars}</p>`;
   console.log(monsterData)
   console.log(monsterData.name)
   console.log(monsterData.weaknesses[0].element)
-  console.log(monsterData.weaknesses[1].stars)
-  
-  // testMonsterName.push(monsterName.name)
-  
+  console.log(monsterData.weaknesses[0].stars)
 }
 
-
-
-function updateMonsterData(testName, weakness) {
-  // monsterTitle.innerHTML = `${monsterName.name}`;
-}
-// console.log(apiUrl);
 getMonster();
-// console.log(testMonsterName)
+
 
  
